4df00ad038266c083f20e7fb3599702810a0dc71
boushib <boushib01@gmail.com>
2020-05-23 19:14:44 +0000

    create a basic resume creation multi-steps form

---
+++ src/main/webapp/assets/js/app.js
@@ -0,0 +1,18 @@
+// create resume form
+const resumeForm = document.querySelector(".create-resume form")
+const resumeFormButton = document.getElementById('create-resume-form-btn')
+const resumeFormSteps = document.querySelectorAll(".create-resume form .step")
+const resumeFormStepsDots = document.querySelectorAll(".create-resume .steps-dots .dot")
+let currentResumeFormStep = 0
+
+resumeFormButton.addEventListener("click", e => {
+  e.preventDefault()
+  currentResumeFormStep++
+
+  if ( currentResumeFormStep === 4 ) return resumeForm.submit()
+
+  resumeFormSteps.forEach(step => step.classList.remove("active"))
+  resumeFormStepsDots.forEach(dot => dot.classList.remove("active"))
+  resumeFormSteps[currentResumeFormStep].classList.add("active")
+  resumeFormStepsDots[currentResumeFormStep].classList.add("active")
+})
